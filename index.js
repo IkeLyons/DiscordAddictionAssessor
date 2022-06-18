2a9965bf6d279b4ba9b34cdb747ad8dddecd07b5
Ike Lyons <lyonsw@rpi.edu>
2022-06-18 15:31:19 -0400

    detect voice activity

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const { Client, Intents } = require("discord.js");
 const { token } = require("./config.json");
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_INTEGRATIONS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_INTEGRATIONS] });
 
 client.once("ready", () => {
 	console.log("Ready!");
 });
 
 client.on("interactionCreate", async (interaction) => {
-	//   if (!interaction.isCommand()) return;
+	if (!interaction.isCommand()) return;
 	console.log(interaction);
 	const { commandName } = interaction;
 
@@ -21,6 +21,17 @@ client.on("interactionCreate", async (interaction) => {
 	else if (commandName === "mytime") {
 		console.log("time");
 		await interaction.reply("mytime");
+	}
+});
+
+client.on('voiceStateUpdate', (oldState, newState) => {
+	// console.log(oldState);
+	// console.log(newState);
+	if (newState.channelId === null) {
+		console.log('user left channel', oldState.channelID);
+	}
+	else if (oldState.channelId === null) {
+		console.log('user joined channel', newState.channelID);
 	}
 });
 
