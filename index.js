34eef1588e9b3e8b47192dea67c0c570420e349b
Ike Lyons <lyonsw@rpi.edu>
2022-06-18 15:06:35 -0400

    trying out eslint

--- index.js
+++ index.js
@@ -4,22 +4,24 @@ const { token } = require("./config.json");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_INTEGRATIONS] });
 
 client.once("ready", () => {
-  console.log("Ready!");
+	console.log("Ready!");
 });
 
 client.on("interactionCreate", async (interaction) => {
-  //   if (!interaction.isCommand()) return;
-  console.log(interaction);
-  const { commandName } = interaction;
+	//   if (!interaction.isCommand()) return;
+	console.log(interaction);
+	const { commandName } = interaction;
 
-  if (commandName === "leaderboard") {
-    await interaction.reply("heres the board!");
-  } else if (commandName === "time") {
-    await interaction.reply("user");
-  } else if (commandName === "mytime") {
-    console.log("time");
-    await interaction.reply("mytime");
-  }
+	if (commandName === "leaderboard") {
+		await interaction.reply("heres the board!");
+	}
+	else if (commandName === "time") {
+		await interaction.reply("user");
+	}
+	else if (commandName === "mytime") {
+		console.log("time");
+		await interaction.reply("mytime");
+	}
 });
 
 client.login(token);
