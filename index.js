63a8e2a3e30666058f32980b033524ed02e270eb
Ike Lyons <lyonsw@rpi.edu>
2022-06-20 01:35:07 -0400

    added spaget for leaderboard

--- index.js
+++ index.js
@@ -19,11 +19,21 @@ client.once("ready", () => {
 
 client.on("interactionCreate", async (interaction) => {
 	if (!interaction.isCommand()) return;
-	console.log(interaction);
+	// console.log(interaction);
 	const { commandName } = interaction;
 
 	if (commandName === "leaderboard") {
-		await interaction.reply("heres the board!");
+		let leaderboard = "";
+		const response = await pool.query(`SELECT user_id, server_id, hours FROM time_spent WHERE(server_id=${interaction.guild.id})ORDER BY hours DESC LIMIT 5`);
+		for (const user of response.rows) {
+			const seconds = Math.floor((user.hours * 60 * 60) % 60);
+			const minutes = Math.floor((user.hours * 60) % 60);
+			const hours = Math.floor(user.hours);
+			const username = await interaction.guild.members.fetch(user.user_id);
+			leaderboard = leaderboard + `#1:${username}\t\t\t${hours} hours,\t${minutes} minutes,\t${seconds} seconds\n`;
+		}
+
+		await interaction.reply("heres the board!\n" + leaderboard);
 	}
 	else if (commandName === "time") {
 		await interaction.reply("user");
@@ -34,21 +44,21 @@ client.on("interactionCreate", async (interaction) => {
 	}
 });
 
+
 // {userid:[timeSpentInMilliseconds, timeJoined]}
 const currentlyConnected = {};
 
 client.on('voiceStateUpdate', (oldState, newState) => {
+	const currentTime = new Date();
 	if (newState.channelId === null) {
 		console.log('user left channel', oldState.channelID);
-		const currentTime = new Date();
-		const timeSpent = Math.abs(currentlyConnected[newState.member.user.id][1] - currentTime);
+		const timeSpent = Math.abs(currentlyConnected[newState.member.user.id][1] - currentTime) / (1000 * 60 * 60);
 		const currentTotal = currentlyConnected[newState.member.user.id][0];
-		// const updatedTime = Math.round(((Math.abs(currentTotal + timeSpent)) / (1000 * 60 * 60)) * 10) / 10;
-		const updatedTime = ((Math.abs(currentTotal + timeSpent)) / (1000 * 60 * 60));
-		pool.query(`UPDATE time_spent SET hours=${updatedTime} WHERE(user_id=${newState.member.user.id} AND server_id=${newState.guild.id})`, (err, res) => {
-			console.log(err, res);
+		const updatedTime = (Math.abs(currentTotal + timeSpent));
+		pool.query(`UPDATE time_spent SET hours=${updatedTime} WHERE(user_id=${newState.member.user.id} AND server_id=${newState.guild.id})`, (err) => {
+			console.log(err);
 		});
-		// console.log(updatedTime);
+		delete currentlyConnected[newState.member.user.id];
 	}
 	else if (oldState.channelId === null) {
 		console.log('user joined channel', newState.channelID);
@@ -56,14 +66,11 @@ client.on('voiceStateUpdate', (oldState, newState) => {
 			console.log(err);
 			if (res.rowCount === 0) {
 				pool.query(`INSERT INTO time_spent (user_id, server_id, hours) VALUES (${newState.member.user.id}, ${newState.guild.id}, 0)`, (err2, res2) => {
-					console.log('Insert');
 					console.log(err2, res2);
 				});
 			}
 			else if (res.rowCount === 1) {
-				const dur = (res.rows[0].hours / (1000 * 60 * 60)) % 24;
-				console.log(dur);
-				currentlyConnected[newState.member.user.id] = [dur, new Date()];
+				currentlyConnected[newState.member.user.id] = [res.rows[0].hours, currentTime];
 			}
 			else {
 				console.log('Duplicate Entry, Something went wrong');
